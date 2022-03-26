107217f3fd79f1aadf92700a640231deeddb30cb
Alice Pote <alice.writes.wrongs@gmail.com>
2022-03-26 14:50:58 -0400

    the config create was causing more trouble than it was worth

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,9 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use config::{Config, ConfigError, Environment, File};
 use handlebars::Handlebars;
 use scraper::{Html, Selector};
-use serde::{Deserialize, Serialize};
+use serde_derive::Serialize;
 use serde_json::json;
 use std::env;
-
-#[derive(Debug, Deserialize)]
-struct Settings {
-    BASE_URL: String,
-}
-
-impl Settings {
-    pub fn new() -> Result<Self, ConfigError> {
-        let s = Config::builder()
-            .add_source(File::with_name("Config.toml").required(false))
-            .add_source(Environment::with_prefix("PROXY"))
-            .build()?;
-
-        s.try_deserialize()
-    }
-}
 
 #[derive(Serialize)]
 struct ScrapeInfo {
@@ -48,10 +31,10 @@ async fn scrape_page_for_main(url: &str) -> Result<ScrapeInfo, Box<dyn std::erro
 #[get("/{path:.*}")]
 async fn get_handler(
     hb: web::Data<Handlebars<'_>>,
-    config: web::Data<Settings>,
+    baseurl: web::Data<String>,
     path: web::Path<String>,
 ) -> impl Responder {
-    let url = config.BASE_URL.clone() + path.as_str();
+    let url = baseurl.get_ref().clone() + path.as_str();
     let scrape_info = scrape_page_for_main(&url).await.unwrap();
     let data = json!(scrape_info);
     let body = hb.render("index", &data).unwrap();
@@ -66,18 +49,18 @@ async fn main() -> std::io::Result<()> {
         .unwrap();
     let handlebars_ref = web::Data::new(handlebars);
 
-    let config = Settings::new().unwrap();
-    let config_ref = web::Data::new(config);
-
     let port = env::var("PORT")
         .unwrap_or_else(|_| "8080".to_string())
         .parse()
         .expect("PORT must be a number");
 
+    let baseurl = env::var("BASE_URL").unwrap();
+    let baseurl_ref = web::Data::new(baseurl);
+
     HttpServer::new(move || {
         App::new()
             .app_data(handlebars_ref.clone())
-            .app_data(config_ref.clone())
+            .app_data(baseurl_ref.clone())
             .service(get_handler)
     })
     .bind(("0.0.0.0", port))?
